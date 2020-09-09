ae7c7fc0a6905d58f93f3bc839a8c07dc293bac2
AzusaChino <azusa146@gmail.com>
2020-09-10 00:03:51 +0800

    ft. 调整pom, 去除flowable

---
+++ little-js/es/iterator.js
@@ -0,0 +1,16 @@
+const arr = ['es5', "es6"]
+
+arr[Symbol.iterator] = function () {
+    let nextIndex = 0;
+    return {
+        next() {
+            return nextIndex < arr.length ? {
+                value: arr[nextIndex++],
+                done: false
+            } : {
+                value: undefined,
+                done: true
+            }
+        }
+    }
+}
