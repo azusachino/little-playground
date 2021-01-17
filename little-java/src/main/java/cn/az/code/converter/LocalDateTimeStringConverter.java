10d2e4dd44b0c468d1225f3addff0925efb6c6bc
AzusaChino <azusa146@gmail.com>
2021-01-17 15:44:12 +0800

    更新java相关依赖的版本

--- little-java/src/main/java/cn/az/code/converter/LocalDateTimeStringConverter.java
+++ little-java/src/main/java/cn/az/code/converter/LocalDateTimeStringConverter.java
@@ -3,6 +3,7 @@ package cn.az.code.converter;
 import cn.hutool.core.convert.Converter;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 /**
  * @author az
@@ -12,6 +13,6 @@ public class LocalDateTimeStringConverter implements Converter<LocalDateTime> {
 
     @Override
     public LocalDateTime convert(Object value, LocalDateTime defaultValue) throws IllegalArgumentException {
-        return null;
+        return Objects.nonNull(value) ? LocalDateTime.parse(value.toString()) : LocalDateTime.now();
     }
 }
