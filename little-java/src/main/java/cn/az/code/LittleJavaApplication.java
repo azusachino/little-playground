39fffef8931d999b19e7e4b47c4a20b441f5fee9
AzusaChino <azusa146@gmail.com>
2021-04-20 19:47:51 +0800

    feat. update es tests

--- little-java/src/main/java/cn/az/code/LittleJavaApplication.java
+++ little-java/src/main/java/cn/az/code/LittleJavaApplication.java
@@ -3,13 +3,15 @@ package cn.az.code;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchRepositoriesAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * @author az
  * @version 2020-03-07
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {ElasticsearchDataAutoConfiguration.class, ElasticsearchRepositoriesAutoConfiguration.class})
 @ServletComponentScan(basePackages = {"cn.az.code.servlet"})
 @MapperScan(basePackages = "cn.az.code.mapper")
 public class LittleJavaApplication {
--- little-java/src/test/java/cn/az/code/ElasticSearchTests.java
+++ little-java/src/test/java/cn/az/code/ElasticSearchTests.java
@@ -1,11 +1,10 @@
 package cn.az.code;
 
-import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.Strings;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
@@ -15,7 +14,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 
 import java.io.IOException;
 
@@ -29,34 +27,40 @@ public class ElasticSearchTests {
     public static final String INDEX = "filebeat-7.12.0-2021.04.18";
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ElasticSearchTests.class);
+
     final private static String[] FETCH_FIELDS = {"@timestamp", "message"};
+
     @Autowired
     RestHighLevelClient restHighLevelClient;
-    @Autowired
-    private ElasticsearchOperations elasticsearchOperations;
 
     @Test
     void search() throws IOException {
-        IndexRequest indexRequest = new IndexRequest(INDEX);
-        IndexResponse indexResponse = restHighLevelClient.index(indexRequest, RequestOptions.DEFAULT);
-        LOGGER.info(String.valueOf(indexResponse.getResult()));
-    }
-
-    @Test
-    void search2() throws IOException {
         SearchRequest sr = new SearchRequest();
         SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
-//        IndexQueryBuilder indexQueryBuilder = new IndexQueryBuilder();
         BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+        // using `must`
         boolQueryBuilder.must(QueryBuilders.matchAllQuery());
-        searchSourceBuilder.query(boolQueryBuilder).fetchSource(FETCH_FIELDS, null);
+        // filter only error message
+        boolQueryBuilder.filter(QueryBuilders.matchQuery("message", "ERROR"));
+
+        // under must using range with customized format
+        boolQueryBuilder.must(QueryBuilders.rangeQuery("@timestamp").gte("prev").lte("next").format("yyyy-MM-dd HH:mm:ss SSS"));
+
+        searchSourceBuilder.query(boolQueryBuilder);
+        searchSourceBuilder.fetchSource(FETCH_FIELDS, Strings.EMPTY_ARRAY);
+        // sort by naming sort field
+        searchSourceBuilder.sort("@timestamp");
+        // using from and size => simulate pagination
+        searchSourceBuilder.from(1);
+        searchSourceBuilder.size(10);
+
         sr.indices(INDEX);
         sr.source(searchSourceBuilder);
 
         SearchResponse searchResponse = restHighLevelClient.search(sr, RequestOptions.DEFAULT);
         if (searchResponse.getHits().getTotalHits().value > 0) {
 
-            System.out.println(searchResponse.getHits().getTotalHits());
+            LOGGER.info("total count: {}", searchResponse.getHits().getTotalHits().value);
 
             for (SearchHit hit : searchResponse.getHits()) {
                 System.out.println("Match: ");
@@ -67,7 +71,6 @@ public class ElasticSearchTests {
         } else {
             System.out.println("No results matching the criteria.");
         }
-
         restHighLevelClient.close();
     }
 }
