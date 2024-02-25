ad898e62a3c86eeed2104fe937c8523b2ff172f9
azusachino <azusachino@proton.me>
2024-02-25 17:00:18 +0900

    feat. WebClient & restTemplate, the same performance (when block)

--- little-webflux/src/main/java/cn/az/webflux/currency/CurrencyRateHandlerChain.java
+++ little-webflux/src/main/java/cn/az/webflux/currency/CurrencyRateHandlerChain.java
@@ -1,6 +1,8 @@
 package cn.az.webflux.currency;
 
 import cn.az.webflux.currency.dto.CurrencyRate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.AnnotationAwareOrderComparator;
 import org.springframework.data.util.Pair;
 
@@ -11,6 +13,8 @@ import java.util.Objects;
  * Currency Handler Chain
  */
 public class CurrencyRateHandlerChain {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CurrencyRateHandlerChain.class);
 
     private final int index;
     private final List<CurrencyRateHandler> handlers;
@@ -34,14 +38,26 @@ public class CurrencyRateHandlerChain {
     public Pair<CurrencyRate, Boolean> getCurrencyRate() {
         if (this.index < this.handlers.size()) {
             var handler = this.handlers.get(this.index);
-            var result = handler.fetchCurrencyRate();
+            CurrencyRate result = null;
+            String simpleName = handler.getClass().getSimpleName();
+
+            LOGGER.info("handler {} started", simpleName);
+            try {
+                result = handler.fetchCurrencyRate();
+            } catch (Exception e) {
+                LOGGER.error("error", e);
+            }
+            LOGGER.info("handler {} finished", simpleName);
+
             if (Objects.nonNull(result)) {
+                LOGGER.info("we got the data from {}", handler.getClass().getSimpleName());
                 return Pair.of(result, Boolean.TRUE);
             } else {
                 var next = new CurrencyRateHandlerChain(this, this.index + 1);
                 return next.getCurrencyRate();
             }
         } else {
+            LOGGER.warn("we got no data this round");
             return Pair.of(CurrencyRate.getInstance(), Boolean.FALSE);
         }
     }
